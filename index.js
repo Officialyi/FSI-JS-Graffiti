e539e3781537c977833a0fc87bd49d441988bd7d
Kevin Yi <officialyi@berkeley.edu>
2022-06-08 18:32:07 -0500

    Update to dogs

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let header = document.querySelector('#page-header');
+header.style.textAlign = "center";
+document.getElementById("Doggos").innerHTML="Old Dogs > Cats";
+header.style.backgroundColor="coral";
 
-// Select some elements...
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
